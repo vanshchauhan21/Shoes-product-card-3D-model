59508b4c5cf94949d8d2f5b08e2a3385da1e8961
Vansh chauhan <143607775+vanshchauhan21@users.noreply.github.com>
2024-04-02 19:37:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.clear();
+
+const elShoes = document.querySelectorAll('.shoe');
+
+[...elShoes].forEach( shoe => {
+  
+  shoe.addEventListener('click',()=>{
+    
+    [...document.querySelectorAll('.shoe[data-active]')].forEach( el => delete el.dataset.active );
+    
+    shoe.dataset.active = true;
+    
+  });
+  
+})
